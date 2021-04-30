e42483b4c24ec7613781a91e7d00e0d9d7cc7f79
Nil Patel <50132600+PatelNil@users.noreply.github.com>
2021-04-30 22:30:10 +0530

    'v0.2'

--- online_census/userInterface/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class StatePopulation(models.Model):
-    state = models.CharField(max_length=50)
-    population = models.IntegerField(default=0)
-    menPopulation = models.IntegerField(default=0)
-    womanPopulation = models.IntegerField(default=0)
